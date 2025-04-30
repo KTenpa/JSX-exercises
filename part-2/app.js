25f40171f1553143d2288bec9a1aaab902830d5e
Karma Tenpa <ktenpa@mac.mynetworksettings.com>
2025-04-29 19:46:14 -0500

    Complete part 2

---
+++ part-2/app.js
@@ -0,0 +1,22 @@
+const App = () => {
+    return <div>
+        <Tweet
+            username="guy123"
+            name="guy"
+            date="4/29/25"
+            message="guy123 is tweeting"
+        />
+        <Tweet
+            username="girl123"
+            name="girl"
+            date="4/30/25"
+            message="girl123 is tweeting"
+        />
+        <Tweet
+            username="randomPerson"
+            name="Rando"
+            date="4/1/25"
+            message="Random is tweeting"
+        />
+    </div>
+}
